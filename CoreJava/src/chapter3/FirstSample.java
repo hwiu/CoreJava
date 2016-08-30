f9cc14b1f42506f8a49f1c32da819a6bbf5147fe
hwin <Administrator@PC201604260958>
2016-08-30 10:56:53 +0800

    commit 0 by hwin

---
+++ CoreJava/src/chapter3/FirstSample.java
@@ -0,0 +1,13 @@
+package chapter3;
+/*
+ * create by hwin in 2016.8.30
+ * first sample of java 
+ */
+public class FirstSample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("We will not use \"Hello World\"");
+	}
+
+}
